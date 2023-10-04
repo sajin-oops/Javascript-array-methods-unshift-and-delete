5048b2837c19aaf126334e0fbb7288162974fe08
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-04 16:49:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//ARRAY METHODS
+let array = ['a','b','c','d','e']
+//PUSH - ADD ELEMENT TO THE END
+console.log(array.push('f'))
+console.log(array)
+console.log(array.pop())
+console.log(array)
+console.log(array.shift())
+console.log(array)
+
+//unshift - adds element to the start of the array
+// and return new length
+console.log(array.unshift('a'))
+console.log(array)
+
+
+//delete
+delete array[1]
+console.log(array)
+
